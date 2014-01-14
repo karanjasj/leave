3d0303184086ca13a0e5a9b0646b7456c6318b0c
brice <jstevekaranja@gmail.com>
2014-01-14 10:10:21 +0300

    initialize repository

---
+++ leave/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leave::Application.config.secret_key_base = '00d806cc900adac0c2faa867e17547d2553917a5490b81e110058387dd643d8cdac53c4a44438682d558dba73a0e332bc527b25766071571122df8b44a6663c7'
---
+++ leave/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Leave::Application.config.session_store :cookie_store, key: '_leave_session'
